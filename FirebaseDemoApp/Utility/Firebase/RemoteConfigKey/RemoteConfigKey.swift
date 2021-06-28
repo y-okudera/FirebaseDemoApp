2162b8b9264cf31e79c666f9762ea68b5c52ac3a
yuoku <appledev.yuoku@gmail.com>
2021-06-29 01:08:03 +0900

    Firebase RemoteConfigのサンプルを実装

---
+++ FirebaseDemoApp/Utility/Firebase/RemoteConfigKey/RemoteConfigKey.swift
@@ -0,0 +1,14 @@
+//
+//  RemoteConfigKey.swift
+//  FirebaseDemoApp
+//
+//  Created by Yuki Okudera on 2021/06/29.
+//
+
+import Foundation
+
+enum RemoteConfigKey {
+    enum StringType: String {
+        case homeBackgroundColor = "home_background_color"
+    }
+}
--- FirebaseDemoApp/ViewController.swift
+++ FirebaseDemoApp/ViewController.swift
@@ -11,9 +11,28 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setUp()
     }
 
+    private func setUp() {
+        // Firebase RemoteConfigから情報を取得する
+        FirebaseHelper.shared.remoteConfig.fetch(withExpirationDuration: TimeInterval(0), completionHandler: { status, error -> Void in
+            if status == .success {
+                print("fetch success")
+            } else {
+                print("fetch failed")
+            }
+
+            if let value = FirebaseHelper.shared.remoteConfig.configValue(forKey: RemoteConfigKey.StringType.homeBackgroundColor.rawValue).stringValue {
+                print("value", value)
+                self.view.backgroundColor = UIColor(hex: value) ?? .white
+            }
+
+            if let err = error {
+                print("ERROR", err)
+            }
+        })
+    }
 
 }
 
